485980bce5d13049d315f8dc4ae59e9bb2db0638
marianopoll <mariampoll98@gmail.com>
2018-09-14 14:52:46 -0300

    otros comentarios

--- src/FM9/src/FM9.c
+++ src/FM9/src/FM9.c
@@ -22,8 +22,10 @@ int main(void) {
 	datosConfigFM9 = read_and_log_config("FM9.config");
 
 	callableRemoteFunctions = dictionary_create();
+	
 
 	dictionary_put(callableRemoteFunctions, "DAM_FM9_handshake", &DAM_FM9_handshake);
+	
 
 	log_info(logger, "Voy a escuchar el puerto: %d", datosConfigFM9->puerto);
 
@@ -32,6 +34,9 @@ int main(void) {
 
 	log_info(logger, "Estoy escuchando el puerto: %d", datosConfigFM9->puerto);
 
+	//conexion al servidor-----------------------------
+	
+	
 	pthread_mutex_init(&mx_main, NULL);
 	pthread_mutex_lock(&mx_main);
 	pthread_mutex_lock(&mx_main);
