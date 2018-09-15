9dab4308f0f597efdc7ff28bba13e5f06b86a622
axelfulop <axelfulop@hotmail.com>
2018-09-15 12:34:38 -0300

    elimino cosas

--- FM9/build/FM9.d
+++
@@ -1,5 +0,0 @@
-build/FM9.d: src/FM9.c src/libFM9.h src/socket.h
-
-src/libFM9.h:
-
-src/socket.h:
--- FM9/build/libFM9.d
+++
@@ -1,5 +0,0 @@
-build/libFM9.d: src/libFM9.c src/libFM9.h src/socket.h
-
-src/libFM9.h:
-
-src/socket.h:
